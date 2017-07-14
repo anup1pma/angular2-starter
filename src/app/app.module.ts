943e965864728e22e1f494953c9b6b15aff8d8f4
Anup <reply2anupsingh@gmail.com>
2017-07-14 02:54:11 -0700

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,9 +25,6 @@ import { AppComponent } from './app.component';
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
 import { AppState, InternalStateType } from './app.service';
 import { HomeComponent } from './home';
-import { AboutComponent } from './about';
-import { NoContentComponent } from './no-content';
-import { XLargeDirective } from './home/x-large';
 
 import '../styles/styles.scss';
 import '../styles/headings.css';
@@ -51,10 +48,7 @@ type StoreType = {
   bootstrap: [ AppComponent ],
   declarations: [
     AppComponent,
-    AboutComponent,
-    HomeComponent,
-    NoContentComponent,
-    XLargeDirective
+    HomeComponent
   ],
   /**
    * Import Angular's modules.
